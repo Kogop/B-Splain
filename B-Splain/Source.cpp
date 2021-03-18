5833c8f597c4d99905cf184bf26844c63dc119a4
Vladislav Nesterov <nesterovvlad100@gmail.com>
2021-03-18 23:01:14 +0300

    ponavoroval sebe labu

--- B-Splain/Source.cpp
+++ B-Splain/Source.cpp
@@ -9,8 +9,8 @@ const double b = 1;//конец
 const double h = (b-a)/n; //h = (b-a)/n   шаг
 const double param = 0.3;//просто параметр для погрешности, зануляя его, мы уменьшаем погрешность
 
-double A[n]; double B[n]; double C[n]; double D[n]; double F[n]; double cMatri4noe[n];
-long double MatrixC[n-1][n];
+double A[n]; double B[n]; double C[n]; double D[n]; double F[n];//double cMatrix[n];
+double C1[n-1][n];
 
 //need this change to push to repository
 double f(double x) {
@@ -19,26 +19,81 @@ double f(double x) {
 }
 //C[0] = 0; C[n] = 0
 
+
+
+
+void Gauss(int k, double Matrix[n - 1][n]) {
+	if (Matrix[k][k] != 1) {
+		double T = Matrix[k][k];
+		for (int j = k; j < n; j++) {//нормирование строки
+			Matrix[k][j] = Matrix[k][j] / T;
+		}
+	}
+	for (int i = 0; i < n - 1; i++) {		//проходим по столбцу
+		if ((Matrix[i][k] != 0) && (i != k)) {
+			double T = Matrix[i][k];
+			Matrix[i][k] = 0;
+			for (int j = k + 1; j < n; j++) {	//проходим по двум строкам и вычитаем их
+				Matrix[i][j] -= Matrix[k][j] * T;
+			}
+		}
+	}
+	if (k < n - 2) {
+		Gauss(k + 1, Matrix);
+	}
+}
+
+
+
 int NeobhodimayaFunksia4tobiVMainNeLezt() {
 	for (int i = 0; i < n; i++)
 	{
 		F[i] = f(a + i * h);
-		cout << F[i] << endl;
+		//cout << F[i] << endl;
 	}
 
-	for (int i = 1; i <= n - 1; i++)
-	{
+	for (int i = 1; i <= n - 1; i++)  // delaem matrix C
+		{
+			for (int j = 0; j < n; j++) {
+				if ((i == j) && (i == 0)) {
+					C1[i][j] = 4;
+					C1[i][j + 1] = 1;
+				}
+				if ((i == j) && (i != 0)) {
+					C1[i][j - 1] = 1;
+					C1[i][j] = 4;
+					C1[i][j + 1] = 1;
+				}
+				if (j == n - 1) {
+					C1[i][j] = 3.0 * (F[i] - 2.0 * F[i + 1] + F[i + 2]) / (h * h);
+				}
+			}
+		
+			Gauss(0, C1);
 
-		cMatri4noe[i - 1] = 3 * (F[i - 1] - 2 * F[i] + F[i + 1]) / pow(h, 2);
-	}
+			//Заполнение коэфф С
+			C[0] = 0;
+			C[n] = 0;
+			for (int i = 1; i < n; i++) {
+				C[i] = C1[i - 1][n - 1];
+			}
+
+			//заполнение А, B, D
+			for (int i = 0; i < n; i++) {
+				A[i] = F[i];
+				B[i] = (F[i + 1] - F[i]) / h - h / 3.0 * (2.0 * C[i] + C[i + 1]);
+				D[i] = (C[i + 1] - C[i]) / 3.0 / h;
+			}
+
+		}
 	for (int i = 0; i < n-1; i++)
 	{
 
 	
 
 		
-	 MatrixC[i][5] = cMatri4noe[i];
-	 cout << MatrixC[i][5] << endl;
+	// C1[i][5] = cMatrix[i];
+	// cout << MatrixC[i][5] << endl;
 	}
 	
 	//rtrtrdrtyrur
